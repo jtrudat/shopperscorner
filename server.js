197399d7fb8cb1ad5995f5fe2c582be89ce79a7e
jeff truitt <jtruitt@gmail.com>
2022-11-21 15:02:19 -0800

    backend mongoDB connected

--- server.js
+++ server.js
@@ -2,13 +2,22 @@
 const express = require('express')
 const app = express()
 const authRoutes = require('./routes/authroutes')
+const mongoose = require('mongoose')
+
 
 //Configuration - PORTS and Server
 require('dotenv').config()
 const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
 
 app.use(authRoutes)
 
-app.listen(PORT, ()=>{
-    console.log(`listening on port ${PORT}`)
+// app.listen(PORT, ()=>{
+//     console.log(`listening on port ${PORT}`)
+// })
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    app.listen(PORT)
+    console.log(`mongodb connected on atlas and server listening on ${PORT}`)
 })
