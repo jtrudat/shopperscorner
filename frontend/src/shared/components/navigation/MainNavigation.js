f0449a2921f12aa322720cb4873708c37d6738fd
jeff truitt <jtruitt@gmail.com>
2022-11-23 00:22:22 -0800

    side window slider drawer added

--- frontend/src/shared/components/navigation/MainNavigation.js
+++ frontend/src/shared/components/navigation/MainNavigation.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MainHeader } from './MainHeader'
 import { Link } from 'react-router-dom'
 import { NavLinks } from './NavLinks'
+import { SideDrawer } from './SideDrawer'
 
 export const MainNavigation = (props)=>{
-    return(
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+
+    return (
+        <React.Fragment>
+        {drawerIsOpen ? (
+        <SideDrawer>
+            <nav className="main-navigation__drawer-nav">
+                <NavLinks />
+            </nav>
+        </SideDrawer>
+        ) : null}
         <MainHeader>
             <button className="main-navigation__menu-btn">
                 <span></span>
@@ -14,9 +25,11 @@ export const MainNavigation = (props)=>{
             <h1 className="main-navigation__title">
                 <Link to="/">Muse & Peruse</Link>
             </h1>
-            <nav>
+            <nav className="main-navigation__header-nav">
                 <NavLinks />
             </nav>
         </MainHeader>
-    )
+        </React.Fragment>
+        
+    );
 }
---
+++ frontend/src/shared/components/navigation/SideDrawer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export const SideDrawer = (props)=>{
+    const content = <aside className="side-drawer">{props.children}</aside>
+    return (
+        ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
+    )
+}
