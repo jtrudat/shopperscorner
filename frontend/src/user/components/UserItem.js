357aa58c2dd6f9232cdeb230151f91bea569871b
jeff truitt <jtruitt@gmail.com>
2022-11-22 20:43:44 -0800

    sass component updates

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -1,11 +1,15 @@
 import { React } from 'react'
+import  Avatar  from '../../shared/components/uielements/Avatar'
+import { Link } from 'react-router-dom'
+import Card from '../../shared/components/uielements/Card'
 
 export const UserItem = (props)=>{
     return(
         <li className="user-item">
-            <div className="user-item__content">
+            <Card className="user-item__content">
+                <Link to={`/${props.id}/places`}>
                 <div className="user-item__image">
-                    <img src={props.image} alt={props.name}></img>
+                    <Avatar image={props.image} alt={props.name}/>
                 </div>
                 <div className="user-item__info">
                     <h2>{props.name}</h2>
@@ -13,8 +17,8 @@ export const UserItem = (props)=>{
                         {props.placeCount} {props.placeCount === 1 ? 'Place': 'Places'}
                     </h3>
                 </div>
-            </div>
-
+                </Link>
+            </Card>
         </li>
     )
 }
--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -3,12 +3,12 @@ import { UsersList } from '../components/UsersLists'
 
 export const Users = ()=>{
     const USERS = [
-    // {
-    //     id: 'u1', 
-    // name: 'Joe Blow', 
-    // image: 'https://placekitten.com/g/200/301', 
-    // places: 3
-    // }
+    {
+        id: 'u1', 
+    name: 'Joe Bloww', 
+    image: 'https://placekitten.com/g/200/310', 
+    places: 4
+    }
 ];
     return (
     <UsersList items={USERS}/>
