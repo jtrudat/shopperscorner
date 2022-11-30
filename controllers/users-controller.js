e0a0d005e8a4dd6a48583b8ac96b60628b361040
jeff truitt <jtruitt@gmail.com>
2022-11-30 15:00:40 -0800

    bcrypt jwt edits

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,26 +1,28 @@
 const User = require('../models/userModel')
 const Topic = require('../models/topicsModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-let DUMMY_USERS =[
-    {
-        id: 'u1',
-        name: ' Joe Blow',
-        email: 'j@gmail.com',
-        password: 'dummyuser'
-    },
-    {
-        id: 'u2',
-        name: ' Moe Doe',
-        email: 'm@gmail.com',
-        password: 'dummyuser2'
-    },
-    {
-        id: 'u3',
-        name: 'Frank Tank',
-        email: 'ft@gmail.com',
-        password: 'dummyuser3'
-    }
-]
+// let DUMMY_USERS =[
+//     {
+//         id: 'u1',
+//         name: ' Joe Blow',
+//         email: 'j@gmail.com',
+//         password: 'dummyuser'
+//     },
+//     {
+//         id: 'u2',
+//         name: ' Moe Doe',
+//         email: 'm@gmail.com',
+//         password: 'dummyuser2'
+//     },
+//     {
+//         id: 'u3',
+//         name: 'Frank Tank',
+//         email: 'ft@gmail.com',
+//         password: 'dummyuser3'
+//     }
+// ]
 
 const getUsers = async (req, res)=>{
     let users = await User.find({}, 'email name image topics')
@@ -54,14 +56,24 @@ const signup = async (req, res)=>{
     // DUMMY_USERS.push(createdUser)
     // res.status(200).json({createdUser})
 
+    let hashedPassword
+    hashedPassword = await bcrypt.hash(password, 4)
+
     const createdUser = new User({
         name : name,
         email: email,
         image: "https://placekitten.com/g/200/310",
-        password : password,
+        password : hashedPassword,
         topics : []
     })
     await createdUser.save()
+
+    let token
+    token = jwt.sign(
+        {userId: createdUser._id},
+         'authenticate'
+         );
+
     res.status(200).json({user: createdUser.toObject()})
 }
 
@@ -71,17 +83,35 @@ const login = async (req, res)=>{
 
     let existingUser = await User.findOne({ email: email})
 
-    if (!existingUser || existingUser.password !== password){
-        res.status(400).json({message: 'sorry cant not verify'})
+    if (!existingUser){
+
+      return  res.status(400).json({message: 'sorry cant not verify'})
     }
-    else{res.status(200).json({user: existingUser.toObject()})}
+    
+        let isValidPassword = false
+        isValidPassword = await bcrypt.compare(password, existingUser.password)
+
+    if (!isValidPassword){
+        return res.json ('invalid')
+    }
+
+        let token
+    token = jwt.sign(
+        {userId: existingUser._id},
+         'authenticate'
+         );
+
+        return res.status(200).json({user: existingUser.toObject()})
+    }
+
+        
 
     // const identifiedUser = DUMMY_USERS.find(u => u.email === email)
     // if (!identifiedUser || identifiedUser.password !== password){
     //     res.status(400).json({message: 'sorry cant not verify'})
     // }
     // else{res.status(200).json(`welcome and logged in for ${identifiedUser.email}`)}
-}
+
 
 exports.getUsers = getUsers
 exports.signup = signup
